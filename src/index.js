dbed3cc31df62c0090a570c6f7befb4bfc8fe15b
Grace Chen <gracesusanchen@gmail.com>
2017-06-24 09:20:08 -0700

    Add basic styling

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const API_KEY = '9622b4723f2375113533c2b614b40';
 
 function renderMeetups(meetups) {
   const template = meetups.reduce((html, meetup, index) => {
+    //<image src="${meetup.key_photo.thumb_link}"/>
     return `${html}\n` +
-      `<p>${index}: ${meetup.name}</p>`;
+      `<tr><td></td><td><a href="${meetup.link}">${meetup.name}</a><td>` +
+      `<td>${meetup.city}</td><td>${meetup.organizer.name}</td></tr>`;
   }, '');
 
-  document.getElementById('container').innerHTML = template;
+  document.getElementById('meetup-list').innerHTML = template;
 }
 
 function fetchMeetups(zip) {
@@ -29,4 +31,11 @@ function fetchMeetups(zip) {
   });
 }
 
-fetchMeetups('94107');
+window.onload = () => {
+  const zipInput = document.getElementById('zip-input');
+  zipInput.addEventListener('keydown', (event) => {
+    if (event.which === 13) {
+      fetchMeetups(zipInput.value);
+    }
+  });
+};
