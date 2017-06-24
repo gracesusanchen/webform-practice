3b0dce4e63c062cefe614575270ab3f1a9e4738a
Grace Chen <gracesusanchen@gmail.com>
2017-06-24 09:43:17 -0700

    Add loading UI

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 const request = require('request');
 const API_KEY = '9622b4723f2375113533c2b614b40';
 
+function meetupTemplate(meetup) {
+  let photo = 'https://secure.meetupstatic.com/s/img/041003812446967856280/logo/svg/logo--script.svg';
+  if (meetup.key_photo) {
+    photo = meetup.key_photo.thumb_link;
+  } else if (meetup.group_photo) {
+    photo = meetup.group_photo.thumb_link;
+  }
+  return `<tr><td><img src=${photo}/></td>` +
+      `<td><a href="${meetup.link}">${meetup.name}</a><td>` +
+      `<td>${meetup.city}</td>` +
+      `<td>${meetup.organizer.name}</td>` +
+    '</tr>';
+}
+
 function renderMeetups(meetups) {
-  const template = meetups.reduce((html, meetup, index) => {
-    //<image src="${meetup.key_photo.thumb_link}"/>
-    return `${html}\n` +
-      `<tr><td></td><td><a href="${meetup.link}">${meetup.name}</a><td>` +
-      `<td>${meetup.city}</td><td>${meetup.organizer.name}</td></tr>`;
-  }, '');
+  const template = meetups.reduce((html, meetup, index) =>
+    `${html}\n` + meetupTemplate(meetup)
+  , '');
 
   document.getElementById('meetup-list').innerHTML = template;
 }
 
+function handleEmptyResponse() {
+  const template = `<tr><td>No results available. Have you entered a valid zipcode?</td></tr>`;
+  document.getElementById('meetup-list').innerHTML = template;
+}
+
+function toggleLoading() {
+  const template = `<tr><td>Loading...</td></tr>`;
+  document.getElementById('meetup-list').innerHTML = template;
+}
+
 function fetchMeetups(zip) {
+  toggleLoading();
   request({
     url: 'https://api.meetup.com/find/groups',
     method: 'GET',
@@ -21,13 +43,18 @@ function fetchMeetups(zip) {
       zip
     }
   }, (err, response, body) => {
-    let parsed = body;
-    try {
-      parsed = JSON.parse(body);
-    } catch (e) {
-      // TODO error handle
+    if (err || response.statusCode !== 200
+      || body.length === 0) {
+      handleEmptyResponse();
+    } else {
+      let parsed = body;
+      try {
+        parsed = JSON.parse(body);
+        renderMeetups(parsed);
+      } catch (e) {
+        handleEmptyResponse();
+      }
     }
-    renderMeetups(parsed);
   });
 }
 
