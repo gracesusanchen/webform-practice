e2d3dcbd29f19f8609ed0b1a4b9fccb08ee127b9
Grace Chen <grace@ludembp.local>
2017-06-24 08:52:30 -0700

    Add fetch and display fetch results from meetup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+const request = require('request');
+const API_KEY = '9622b4723f2375113533c2b614b40';
+
+function renderMeetups(meetups) {
+  const template = meetups.reduce((html, meetup, index) => {
+    return `${html}\n` +
+      `<p>${index}: ${meetup.name}</p>`;
+  }, '');
+
+  document.getElementById('container').innerHTML = template;
+}
+
+function fetchMeetups(zip) {
+  request({
+    url: 'https://api.meetup.com/find/groups',
+    method: 'GET',
+    qs: {
+      key: API_KEY,
+      zip
+    }
+  }, (err, response, body) => {
+    let parsed = body;
+    try {
+      parsed = JSON.parse(body);
+    } catch (e) {
+      // TODO error handle
+    }
+    renderMeetups(parsed);
+  });
+}
+
+fetchMeetups('94107');
